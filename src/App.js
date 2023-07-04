b106953baaa25480c12c3a83a6191d90bedaa8e5
Kangyongsoo <aodtn2006@naver.com>
2023-07-04 16:27:24 +0900

    글로벌스타일

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Ranking from "./Ranking";
-import Home from "./Home";
+import GlobalStyle from "./GlobalStyle";
+import Ranking from "./Page/Ranking";
+import Home from "./Page/Home";
+import Profile from "./Page/Profile";
+import Quiz from "./Page/Quiz";
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/ranking" Component={Ranking} />
+        <Route path="/quiz/:category" element={<Quiz />} />
+        <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </div>
   );
--- src/Page/Ranking.js
+++ src/Page/Ranking.js
@@ -1,7 +1,7 @@
 export default function Ranking() {
   return (
-    <div>
-      <div>Ranking</div>
-    </div>
+    <>
+      <></>
+    </>
   );
 }
